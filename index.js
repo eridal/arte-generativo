9b7830aac38189abbdaedad277a241fe90e4d4bf
Martin Scotta <martinscotta@gmail.com>
2016-08-28 18:10:50 -0300

    base

---
+++ index.js
@@ -0,0 +1,12 @@
+
+window.addEventListener('load', function () {
+
+  var canvas = document.getElementById('canvas')
+  var ctx = canvas.getContext('2d')
+
+  main(ctx)
+})
+
+function main (ctx) {
+  console.log(ctx)
+}
